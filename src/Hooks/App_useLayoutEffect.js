5ac875122698f23c4dfdbdf21de9e4c0a59bbe19
Mohamed Abdel Nasser <mohdabdellnasser1@gmail.com>
2020-02-14 01:53:55 +0200

    Implement useLayoutEffect hook

---
+++ src/Hooks/App_useLayoutEffect.js
@@ -0,0 +1,39 @@
+import React, { useState, useLayoutEffect, useRef, useCallback } from 'react';
+import { Button } from './Button';
+import { UseLayoutEffectEx } from './UseLayoutEffectEx';
+
+export default function App() {
+  const [counter, setCounter] = useState(0);
+const [divData, setDivData] = useState({});
+const divRef = useRef();
+const [listOfCount, setListOfCount] = useState([1]);
+
+useLayoutEffect(() => {
+    setDivData(divRef.current.getBoundingClientRect());
+  }, [listOfCount]);
+const handleAddButtons = () => {
+    setListOfCount([...listOfCount, 100]);
+  };
+
+  const onClick = useCallback((n) => setCounter((c) => c + n), [setCounter]);
+
+  return (
+    <div className="container text-center pt-5">
+<Button label="add more" onClick={handleAddButtons} />
+<div ref={divRef}>
+  {listOfCount.map((count) => (
+    <>
+      <Button counterFn={onClick} label={count} n={count} key={count} />
+      <br />
+    </>
+  ))}
+</div>
+<h1 className="mt-3">{counter}</h1>
+{/* <pre style={{ textAlign: 'left' }}>
+  {JSON.stringify(divData, null, 2)}
+</pre> */}
+
+<UseLayoutEffectEx />
+</div>
+);
+}
--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import App_useState from './Hooks/App_useState'
 import App_useEffect from './Hooks/App_useEffect'
 import App_useRef from './Hooks/App_useRef'
 import App from './Hooks/App_useCallback'
+import App from './Hooks/App_useLayoutEffect'
 */
 ReactDOM.render(<App />, document.getElementById('root'));
 
